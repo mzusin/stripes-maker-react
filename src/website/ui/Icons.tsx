2d81833f659b160fa5681e2ae74b8bc947825bc7
Miriam Zusin <miriamzusin@gmail.com>
2023-04-28 22:54:15 +0300

    delete stripe

--- src/website/ui/Icons.tsx
+++ src/website/ui/Icons.tsx
@@ -89,3 +89,27 @@ export const ExportIcon = () => {
         </svg>
     )
 };
+
+export const CloseIcon = () => {
+  return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+           viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round"
+           strokeLinejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M18 6l-12 12"></path>
+          <path d="M6 6l12 12"></path>
+      </svg>
+  )
+};
+
+export const PlusIcon = () => {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="24"
+           height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none"
+           strokeLinecap="round" strokeLinejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M12 5l0 14"></path>
+          <path d="M5 12l14 0"></path>
+      </svg>
+  )
+};
--- src/website/ui/panels/StripesPanel.tsx
+++ src/website/ui/panels/StripesPanel.tsx
@@ -2,7 +2,8 @@ import React, { Fragment } from 'react';
 import { CloseMobileMenuButton, ColorPicker, EButtonType, HSplitter, NumberTextBox, Panel, PanelTitle, PanelSection, Button } from 'darkly';
 import { rootActions, useAppDispatch, useAppSelector } from '../../dal/store';
 import { IStripe } from '../../iterfaces';
-import { addStripe, updateStripe } from '../../domain/stripes-provider';
+import { addStripe, deleteStripe, updateStripe } from '../../domain/stripes-provider';
+import { CloseIcon, PlusIcon } from '../Icons';
 
 const StripesPanel = () => {
 
@@ -47,6 +48,16 @@ const StripesPanel = () => {
         )
     };
 
+    const deleteStripeHandler = (id: string) => {
+        const updatedStripes = deleteStripe(id, stripes);
+
+        dispatch(
+            rootActions.main({
+                stripes: updatedStripes,
+            })
+        )
+    };
+
     return (
         <Panel slideOnMobile={ true }>
             <PanelTitle>
@@ -60,9 +71,28 @@ const StripesPanel = () => {
                 paddingBottom: '250px',
             }}>
                 {
-                    stripes.map(stripe => {
+                    stripes.map((stripe, i) => {
                         return (
                             <Fragment key={ stripe.id }>
+
+                                <section className="flex items-center justify-between mx-4 mt-4 text-slate-500">
+                                    <div className="text-sm">Stripe { i + 1 }</div>
+                                    <button type="button" onClick={ () => {
+                                        deleteStripeHandler(stripe.id);
+                                    }}><CloseIcon /></button>
+                                </section>
+
+                                <ColorPicker
+                                    classes="m-4"
+                                    color={ stripe.color }
+                                    popupPosition="right"
+                                    buttonWidth="100px"
+                                    setColor={ (newColor: string) => {
+                                        onColorChange(stripe, newColor);
+                                    }}>
+                                    Color
+                                </ColorPicker>
+
                                 <NumberTextBox
                                     classes="m-4"
                                     width="100px"
@@ -73,16 +103,6 @@ const StripesPanel = () => {
                                     Size
                                 </NumberTextBox>
 
-                                <ColorPicker
-                                    classes="m-4"
-                                    color={ stripe.color }
-                                    popupPosition="right"
-                                    setColor={ (newColor: string) => {
-                                        onColorChange(stripe, newColor);
-                                    }}>
-                                    Color
-                                </ColorPicker>
-
                                 <HSplitter />
                             </Fragment>
                         )
@@ -91,14 +111,8 @@ const StripesPanel = () => {
             </PanelSection>
 
             <Button type={ EButtonType.Primary } classes="my-4" onClick={ addStripeHandler }>
-                Add Stripe
-                <svg xmlns="http://www.w3.org/2000/svg" className="ml-4" width="24"
-                     height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none"
-                     strokeLinecap="round" strokeLinejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M12 5l0 14"></path>
-                    <path d="M5 12l14 0"></path>
-                </svg>
+                <span className="mr-2">Add Stripe</span>
+                <PlusIcon />
             </Button>
         </Panel>
     )
