2564bdc01fbacbe11eb0ff73aa301cfc7779d2a9
Miriam Zusin <miriamzusin@gmail.com>
2023-04-28 17:14:52 +0300

    svg defaults

--- src/website/App.tsx
+++ src/website/App.tsx
@@ -5,8 +5,15 @@ import ActionsPanel from './ui/ActionsPanel';
 import MainTopBar from './ui/MainTopBar';
 import SVG from './ui/SVG';
 import MainBottomBar from './ui/MainBottomBar';
+import { EAnimationType } from './iterfaces';
+import { useAppSelector } from './dal/store';
 
 const App = () => {
+
+    const stripes = useAppSelector(store => store.root.stripes);
+    const lineRotation = useAppSelector(store => store.root.lineRotation);
+    const animationType = useAppSelector(store => store.root.animationType);
+    const animationDuration = useAppSelector(store => store.root.animationDuration);
 
     return (
         <div className="bg-slate-900 text-slate-200 h-screen w-full flex text-base">
@@ -19,7 +26,12 @@ const App = () => {
 
                 <MainTopBar />
 
-                <SVG />
+                <SVG
+                    stripes={ stripes }
+                    lineRotation={ lineRotation }
+                    animationType={ animationType }
+                    animationDuration={ animationDuration }
+                />
 
                 <MainBottomBar />
             </div>
--- src/website/dal/root-slice.ts
+++ src/website/dal/root-slice.ts
@@ -1,8 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { EAction } from '../iterfaces';
+import { EAction, EAnimationType, IStripe } from '../iterfaces';
+import { newId } from 'mz-math';
 
 export interface IRootState{
     selectedAction: EAction;
+
+    lineRotation: number;
+    animationType: EAnimationType;
+    animationDuration: number;
+    stripes: IStripe[];
 }
 
 /**
@@ -10,6 +16,40 @@ export interface IRootState{
  */
 const initialState: IRootState = {
     selectedAction: EAction.ImageSize,
+
+    /*out: './out/example.svg',  // the pass to the output file
+    width: 300, // SVG width
+    height: 300, // SVG height
+    bgColor: '#fff', // SVG background color*/
+    lineRotation: 45, // stripes rotation angle in degrees
+
+    // optional animation -----------
+    animationType: EAnimationType.Linear,
+    animationDuration: 500, // seconds
+
+    // the list of stripes ----------
+    stripes: [
+        {
+            color: '#505050',
+            size: 20,
+            id: newId(),
+        },
+        {
+            color: '#92CBFA',
+            size: 10,
+            id: newId(),
+        },
+        {
+            color: '#AB3C83',
+            size: 20,
+            id: newId(),
+        },
+        {
+            color: '#ffffff',
+            size: 10,
+            id: newId(),
+        }
+    ]
 };
 
 const rootSlice = createSlice({
